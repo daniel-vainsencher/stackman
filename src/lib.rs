101fc77135762f137cfc9c601a068d768c833499
Daniel Vainsencher <daniel.vainsencher@princeton.edu>
2015-09-30 01:30:46 -0400

    Wrap precise_time_ns to use .

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
 extern crate time;
+use time::precise_time_ns;
+
+fn time_ns() -> u64 {
+    precise_time_ns()
+}
 
 #[macro_export]
 macro_rules! stack {
     ($name:expr, $work:expr) => {
         {
-            use time::precise_time_ns;
             let work = {|| $work};
             println!("Push {}", $name);
-            let before = precise_time_ns();
+            let before = $crate::time_ns();
             let value = work();
-            let after = precise_time_ns();
+            let after = $crate::time_ns();
             println!("Pop {} spent {}ns.", $name, after-before);
             value
         }
