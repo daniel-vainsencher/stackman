dad1c7c3462017f6d83fc72e75930e3da880303c
Daniel Vainsencher <daniel.vainsencher@princeton.edu>
2015-09-30 11:32:44 -0400

    write to stderr instead of stdout

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate time;
 use time::precise_time_ns;
-
 pub fn time_ns() -> u64 {
     precise_time_ns()
 }
@@ -9,12 +8,14 @@ pub fn time_ns() -> u64 {
 macro_rules! stack {
     ($name:expr, $work:expr) => {
         {
+            use std::io::Write;
+            let mut stderr = std::io::stderr();
             let mut work = {|| $work};
-            println!("Push {}", $name);
+            writeln!(&mut stderr, "Push {}", $name).unwrap();
             let before = $crate::time_ns();
             let value = work();
             let after = $crate::time_ns();
-            println!("Pop {} spent {}ns.", $name, after-before);
+            writeln!(&mut stderr, "Pop {} spent {}ns.", $name, after-before).unwrap();
             value
         }
     }
